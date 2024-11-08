cba97e89175e44a90a5512fe96f39fbb12055de9
devfast01 <iot123tech@gmail.com>
2024-11-08 13:09:11 +0500

    tutorial 3 was done

--- lib/tutorial_3/tutorial_3_main.dart
+++ lib/tutorial_3/tutorial_3_main.dart
@@ -41,88 +41,86 @@ class MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if (state.wasInremented) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Incremented'),
-                duration: Duration(
-                    milliseconds:
-                        300), // Use milliseconds instead of microseconds for better visibility
-              ),
-            );
-          } else if (!state.wasInremented) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Decremented'),
-                duration: Duration(
-                    milliseconds:
-                        300), // Use milliseconds instead of microseconds for better visibility
-              ),
-            );
-          }
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              BlocBuilder<CounterCubit, CounterState>(
-                builder: (context, state) {
-                  if (state.counterValue < 0) {
-                    return Text(
-                      'ops, NEGATIVE ${state.counterValue}',
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    );
-                  } else if (state.counterValue % 2 == 0) {
-                    return Text(
-                      'yeah ${state.counterValue}',
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    );
-                  } else if (state.counterValue == 5) {
-                    return Text(
-                      'okay, NUMBER 5',
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    );
-                  } else
-                    // ignore: curly_braces_in_flow_control_structures
-                    return Text(
-                      state.counterValue.toString(),
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    );
-                },
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FloatingActionButton(
-                    heroTag: Text(widget.title),
-                    onPressed: () {
-                      BlocProvider.of<CounterCubit>(context).decrement();
-                      // context.read<CounterCubit>().decrement();
-                    },
-                    tooltip: 'Decrement',
-                    child: const Icon(Icons.remove),
-                  ),
-                  FloatingActionButton(
-                    heroTag: Text('${widget.title} #2'),
-                    onPressed: () {
-                      BlocProvider.of<CounterCubit>(context).increment();
-                      // context.read<CounterCubit>().increment();
-                    },
-                    tooltip: 'Increment',
-                    child: const Icon(Icons.add),
-                  ),
-                ],
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasInremented) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Incremented'),
+                      duration: Duration(
+                          milliseconds:
+                              300), // Use milliseconds instead of microseconds for better visibility
+                    ),
+                  );
+                } else if (!state.wasInremented) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Decremented'),
+                      duration: Duration(
+                          milliseconds:
+                              300), // Use milliseconds instead of microseconds for better visibility
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'ops, NEGATIVE ${state.counterValue}',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                } else if (state.counterValue % 2 == 0) {
+                  return Text(
+                    'yeah ${state.counterValue}',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                } else if (state.counterValue == 5) {
+                  return Text(
+                    'okay, NUMBER 5',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                } else
+                  // ignore: curly_braces_in_flow_control_structures
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+              },
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  heroTag: Text(widget.title),
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                    // context.read<CounterCubit>().decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  heroTag: Text('${widget.title} #2'),
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                    // context.read<CounterCubit>().increment();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
