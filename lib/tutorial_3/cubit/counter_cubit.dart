5a5517cbdf82cbac339cb6af686dc37fb72b458a
devfast01 <iot123tech@gmail.com>
2024-11-08 13:07:52 +0500

    tutorial 3 was done

--- lib/tutorial_3/cubit/counter_cubit.dart
+++ lib/tutorial_3/cubit/counter_cubit.dart
@@ -6,8 +6,8 @@ class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counterValue: 0));
 
   void increment() => emit(
-      CounterState(counterValue: state.counterValue + 1));
+      CounterState(counterValue: state.counterValue + 1, wasInremented: true));
 
-  void decrement() => emit(CounterState(
-      counterValue: state.counterValue - 1));
+  void decrement() => emit(
+      CounterState(counterValue: state.counterValue - 1, wasInremented: false));
 }
--- lib/tutorial_3/cubit/counter_state.dart
+++ lib/tutorial_3/cubit/counter_state.dart
@@ -2,8 +2,6 @@ part of 'counter_cubit.dart';
 
 class CounterState {
   int counterValue;
-
-  CounterState({
-    required this.counterValue
-  });
+  bool wasInremented;
+  CounterState({required this.counterValue, this.wasInremented = false});
 }
